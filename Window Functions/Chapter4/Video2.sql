84a123808c67054a31a557bb5569ec805f18568b
ZzzhangXiao <161013045+ZzzhangXiao@users.noreply.github.com>
2024-06-09 00:31:56 +0800

    Update Video2.sql

--- Window Functions/Chapter4/Video2.sql
+++ Window Functions/Chapter4/Video2.sql
@@ -10,11 +10,13 @@ SELECT 	*
 FROM	routine_checkups;
 
 -- Average species heart rates
+-- CAST: convert the result of AVG to "AS DECIMAL --
+-- (5,2): 5 digits(left + right of dp), 2 scale (2dp) --
 SELECT 	species, 
 		name,
 		checkup_time, 
 		heart_rate,
-		CAST (
+		CAST ( 
 				AVG (heart_rate) 
 				OVER (PARTITION BY species)
 			 AS DECIMAL (5, 2)
@@ -28,7 +30,7 @@ SELECT 	species,
 		name, 
 		checkup_time, 
 		heart_rate,
-		EVERY (Heart_rate >= 	AVG (heart_rate) 
+		EVERY (Heart_rate >= 	AVG (heart_rate)  -- cannot nest window function
 								OVER (PARTITION BY species)
 				) 
 		OVER (PARTITION BY species, name) AS consistently_at_or_above_average
@@ -36,7 +38,24 @@ FROM	routine_checkups
 ORDER BY 	species ASC,
 			checkup_time ASC;
 
--- Split with CTE
+-- Split with CTE: CTE creates a temporary set of table can be used in other SELECT --
+-- Example -- 
+-- WITH sample_cte 
+-- AS (
+-- SELECT 
+--    	column1, 
+-- 	column2 
+-- FROM 
+--	your_table
+-- WHERE 
+-- 	condition = 'something'
+-- )
+-- SELECT 
+-- 	column1, 
+-- 	column2
+-- FROM 
+-- 	sample_cte;
+
 WITH species_average_heart_rates
 AS
 (
@@ -83,8 +102,8 @@ WHERE 	EVERY (heart_rate >= species_average_heart_rate)
 ORDER BY 	species ASC,
 			checkup_time ASC;
 
--- Separate into CTEs
-WITH species_average_heart_rates
+-- !!!!! Separate into CTEs
+WITH species_average_heart_rates -- first CTE
 AS
 (
 SELECT 	species, 
@@ -105,15 +124,15 @@ SELECT	species,
 		checkup_time, 
 		heart_rate,
 		species_average_heart_rate,
-		EVERY (heart_rate >= species_average_heart_rate) 
+		EVERY (heart_rate >= species_average_heart_rate) -- after splitting to CTE, avoid the issue of cannot nest AVG in this window function
 		OVER (PARTITION BY species, name) AS consistently_at_or_above_average
 FROM 	species_average_heart_rates
 )
-SELECT 	DISTINCT species,
+SELECT 	DISTINCT species, -- unique combination of all columns specified here
 		name,
 		heart_rate,
 		species_average_heart_rate
-FROM 	with_consistently_at_or_above_average_indicator
+FROM 	with_consistently_at_or_above_average_indicator -- can refer to the defined CTE here
 WHERE 	consistently_at_or_above_average
 ORDER BY 	species ASC,
 			heart_rate DESC;
